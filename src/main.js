6fad8ad23b5ddf8526a52179918d9b0cf45be0bb
vrbdas <120019157+vrbdas@users.noreply.github.com>
2025-05-05 13:32:43 +0500

    add media and menu

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import form from './modules/form.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   modal('.subscribe__overlay', ['.header__subscribe', '.footer__top-link'], '.subscribe__close');
+  modal('.menu__overlay', '.header__hamburger', '.menu__close');
   form();
 });
